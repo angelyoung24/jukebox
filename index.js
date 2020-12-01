7cdb7b7b65b9a49dc3259f10749915b43e565bc4
Angel Young <angelyoung@Angels-iMac.local>
2020-12-01 00:08:08 -0500

    updated to es6

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var project = new Audio();
+const project = new Audio();
 i = 0;
 
-var song = ["audio/Don_t_Look.mp3", "audio/Don_t_Turn_Back.mp3", "audio/Sweet_and_Low.mp3", "audio/The_Bluest_Star.mp3", "audio/The_Coldest_Shoulder.mp3"]
+let song = ["audio/Don_t_Look.mp3", "audio/Don_t_Turn_Back.mp3", "audio/Sweet_and_Low.mp3", "audio/The_Bluest_Star.mp3", "audio/The_Coldest_Shoulder.mp3"]
 
-var songTitle = document.getElementById("songtitle");
-var playButton = document.getElementById("play").addEventListener("click", playAudio);
-var pauseButton = document.getElementById("pause").addEventListener("click", pauseAudio);
-var stop = document.getElementById("stop").addEventListener("click", stopAudio);
-var next = document.getElementById("next").addEventListener("click", nextSong);
+const songTitle = document.getElementById("songtitle");
+const playButton = document.getElementById("play").addEventListener("click", playAudio);
+const pauseButton = document.getElementById("pause").addEventListener("click", pauseAudio);
+const stop = document.getElementById("stop").addEventListener("click", stopAudio);
+const next = document.getElementById("next").addEventListener("click", nextSong);
 
 
 function playAudio(){
